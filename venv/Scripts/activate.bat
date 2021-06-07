401e26ae481ef94069dc5756f7bab9777e3cd439
Александр Пыжьянов <50012164+Artecter@users.noreply.github.com>
2021-06-07 21:21:20 +0500

    0.1
    Version 0.1

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Artecter\OneDrive\Documents\2 Python OOP\venv
+set VIRTUAL_ENV=D:\Artecter\GitHub\PythonBase\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
