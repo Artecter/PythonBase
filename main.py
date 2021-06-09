2f0479988cf311adc0074f399a7eb66dd7deb18c
Александр Пыжьянов <50012164+Artecter@users.noreply.github.com>
2021-06-09 18:20:10 +0500

    0.2.5
    Version 0.2.5

--- main.py
+++ main.py
@@ -5,6 +5,25 @@ from base import base_method
 import sys
 
 
+class Window(QMainWindow):
+    def __init__(self):
+        super(Window, self).__init__()
+
+        self.setWindowTitle("Python Base")
+        self.setGeometry(300, 250, 350, 200)
+
+        self.main_text = QtWidgets.QLabel(self)
+        self.main_text.setText("Работает!")
+        self.main_text.move(100, 100)
+        self.main_text.adjustSize()
+
+        self.btn = QtWidgets.QPushButton(self)
+        self.btn.move(70, 150)
+        self.btn.setText("Нажать!")
+        self.btn.setFixedWidth(200)
+        self.btn.clicked.connect(add_label)
+
+
 def add_label():
     base_method()
 
@@ -12,19 +31,6 @@ def add_label():
 def application():
     app = QApplication(sys.argv)
     window = QMainWindow()
-
-    window.setWindowTitle("Python Base")
-    window.setGeometry(300, 250, 350, 200)
-
-    main_text = QtWidgets.QLabel(window)
-    main_text.setText("Работает!")
-    main_text.move(100, 100)
-    main_text.adjustSize()
-
-    btn = QtWidgets.QPushButton(window)
-    btn.move(70, 150)
-    btn.setText("Нажать!")
-    btn.setFixedWidth(200)
 
     window.show()
     sys.exit(app.exec_())
